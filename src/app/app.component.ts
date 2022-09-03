0eb35886730ec5aa8baa1c5ac1f63a5dae2d62c7
Steve Snyder <ssnyde@gmail.com>
2022-09-02 20:52:46 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'bt_scan_log_webapp_v1';
+}
