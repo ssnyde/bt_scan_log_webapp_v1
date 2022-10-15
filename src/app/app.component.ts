4c3aee772c43bdc9883dba39cc67af76d2c49278
Steve Snyder <ssnyde@gmail.com>
2022-10-14 23:27:08 -0400

    Got table loading restaurants from API and dynamically updated.  Next either add pagination sorting and filtering based on example code, or update the API to match the BLE scanner data.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,26 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { APIService, Restaurant } from './API.service';
 import { Subscription } from 'rxjs';
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-root',
@@ -29,8 +10,8 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit, OnDestroy {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
+  displayedColumns: string[] = ['id', 'name', 'description', 'city'];
+  dataSource = new MatTableDataSource<Restaurant>();
   title = 'bt_scan_log_webapp_v1';
 
   public createForm: FormGroup;
@@ -52,13 +33,15 @@ export class AppComponent implements OnInit, OnDestroy {
     /* fetch restaurants when app loads */
     this.api.ListRestaurants().then((event) => {
       this.restaurants = event.items as Restaurant[];
+      this.dataSource.data = this.restaurants;
     });
 
     /* subscribe to new restaurants being created */
+    this.dataSource.data = this.restaurants;
     this.subscription = <Subscription>(
       this.api.OnCreateRestaurantListener.subscribe((event: any) => {
-        const newRestaurant = event.value.data.onCreateRestaurant;
-        this.restaurants = [newRestaurant, ...this.restaurants];
+        this.restaurants = [event.value.data.onCreateRestaurant, ...this.restaurants];
+        this.dataSource.data = this.restaurants;
       })
     );
   }
